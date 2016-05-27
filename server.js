eecef4787de13b0e425a998ea5938a57bd44be80
Jasson Cascante <jasson.cascante@edify.cr>
2016-05-26 20:45:09 -0600

    minor tweaks to support heroku

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-var theme = require('./lib/theme');
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var app = express();
-var minify = require('express-minify');
-var compress = require('compression');
+var theme = require('./lib/theme')
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var app = express()
+var minify = require('express-minify')
+var compress = require('compression')
 
-app.use(compress());
-app.use(minify(
-{
+app.use(compress())
+app.use(minify({
   cache: __dirname + '/cache'
-}));
+}))
 
 app.use(bodyParser.json())
-app.use(cors());
+app.use(cors())
 
-app.use("/themes.json", express.static('themes.json'));
-app.get('/:theme', theme);
-app.post('/:theme', theme);
+app.use('/themes.json', express.static('themes.json'))
+app.get('/:theme', theme)
+app.post('/:theme', theme)
 
-app.get('/theme/:theme', theme);
-app.post('/theme/:theme', theme);
+app.get('/theme/:theme', theme)
+app.post('/theme/:theme', theme)
 
-app.listen(3000);
+app.listen(process.env.PORT || 5000)
 
-console.log("Starting theme-manager..");
-console.log('Server is now running at http://localhost:3000/');
+console.log('Starting theme-manager..')
+console.log('Server is now running at http://localhost:3000/')
